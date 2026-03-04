0881fc74af64cbfa5799a2ae128f00a5c58c5628
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2026-03-03 23:35:17 -0300

    Add Graph class interface and partial impl
    Introduce Graph.h with members and method declarations (constructor, addEdge, getNumVertices, degree, getNeighbors, degreeSequence, numberOfEdges, print). Implement Graph::Graph(int) and Graph::addEdge in Graph.cpp and add necessary includes. Also add IDE project dictionary and two built binaries (main.exe) under Implementations/src and output/Implementations/src. Note: several Graph methods are declared but not yet implemented.

--- Implementations/include/Graph.h
+++ Implementations/include/Graph.h
@@ -5,9 +5,28 @@
 #ifndef IMPLEMENTATIONS_GRAPH_H
 #define IMPLEMENTATIONS_GRAPH_H
 
+#include <vector>
 
 class Graph {
-};
+    private:
+    int n;
+    std::vector<std::vector<int> > adj;
+    
+    public: 
+    Graph(int vertices);
 
+    void addEdge(int u, int v);
+
+    int getNumVertices() const;
+
+    int degree() const;
+
+    std::vector<int> getNeighbors(int v) const;
+    std::vector<int> degreeSequence() const;
+
+    int numberOfEdges() const;
+
+    void print() const;
+};
 
 #endif //IMPLEMENTATIONS_GRAPH_H
--- Implementations/src/Graph.cpp
+++ Implementations/src/Graph.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "../include/Graph.h"
+#include <iostream>
+#include <algorithm>
+
+Graph::Graph(int vertices)
+    : n(vertices) {
+    adj.resize(n);
+}
+void Graph::addEdge(int u, int v){
+    adj[u].push_back(v);
+};
+
